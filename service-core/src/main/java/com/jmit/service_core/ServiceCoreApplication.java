215ed4ab8720719dd7f4a7fc29222a006ed16113
飞翔天鸟 <1760337415@qq.com>
2021-07-13 13:49:08 +0800

    提交文件

---
+++ service-core/src/main/java/com/jmit/service_core/ServiceCoreApplication.java
@@ -0,0 +1,17 @@
+package com.jmit.service_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName:ServiceCoreApplication
+ * @Author:LiuGang
+ * @Date:2021/7/13 10:35
+ * @Version: v1.0
+ **/
+@SpringBootApplication
+public class ServiceCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCoreApplication.class,args);
+    }
+}
